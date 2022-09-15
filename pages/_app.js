d792f0ecfb117b8f5f0766994e9fd780224b1993
Sakura Tanaka <sakuratanaka@sakuranoair.localdomain>
2022-09-15 20:46:53 +0200

    added favicon

--- pages/_app.js
+++ pages/_app.js
@@ -2,11 +2,33 @@ import "../styles/globals.css";
 import Footer from "../components/Footer";
 import Aside from "../components/Aside";
 import Header from "../components/Header";
+import Link from "next/link";
+import Head from "next/head";
 
 function MyApp({ Component, pageProps }) {
   return (
     <>
-      <Header />
+      <Head>
+        <link
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href="/apple-touch-icon.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href="/favicon-32x32.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+          href="/favicon-16x16.png"
+        />
+        <link rel="manifest" href="/site.webmanifest" />
+      </Head>
+
       <main className="main">
         <Aside />
 
